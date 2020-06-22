99233ac8666cc08242b9785d89eedbf30a53b37c
Connor <cfech44@gmail.com>
2020-06-21 22:45:04 -0400

    adding comments:

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-
+//event listener for document
 document.addEventListener("keyup", whatKey)
 
+// vars for html elements 
 var keyDiv = document.getElementById("keySpan")
 var keyCodeDiv = document.getElementById("keyCodeSpan")
 var historyDiv = document.getElementById("history")
+
+//previous key clicked array 
 var previousKC = []
 
 
+// function to get the key code for key clicked 
 function whatKey(e) {
     console.log(e.keyCode)
     console.log(e)
@@ -15,42 +19,50 @@ function whatKey(e) {
     var kc = e.keyCode
     var k = e.key
 
+    //validation for spaces
+    if (e.key === " ") {
+        k = "Space"
+    }
 
-
+// setting the html text content
     keyDiv.textContent = k
     keyCodeDiv.textContent = kc
 
+    //building out key combo object for history 
     let keyCombo = {
         key: k,
         keyC: kc
     }
 
-    console.log("looking not for meta: ", keyCombo.key)
+    // pushing the keyCombo to the array for further use
     previousKC.push(keyCombo)
-    console.log("whatKey -> previousKC", previousKC)
+
+    // calling the set history function
     setHistory(previousKC)
-
-
 }
 
+
 const setHistory = (array) => {
+    // resetting the historyDiv
     historyDiv.textContent = ""
+
+    //creating a list and adding a class
     var list = document.createElement("ol")
     list.classList.add("listStyle")
-    // list.textContent=  ""
-    for (let i = 0; i<array.length; i++ ){
+
+    //looping through our array of objects and setting the text content of previously clicked keys 
+    for (let i = 0; i < array.length; i++) {
+
         let listItem = document.createElement("li")
         listItem.classList.add("listItemStyle")
+
         let key2 = JSON.stringify(array[i].key)
         let keyCode2 = JSON.stringify(array[i].keyC)
-        console.log("setHistory -> keyCode2", keyCode2)
-        console.log("setHistory -> key2", key2)
-        console.log("KEYYYYY",JSON.stringify(array[i].key))
+        
         listItem.textContent = `KEY: ${key2}, KEYCODE: ${keyCode2}`
         list.prepend(listItem)
     }
-    // console.log(list)
-    console.log(historyDiv)
-    historyDiv.append(list)
 
+// appending our list to the history div
+    historyDiv.append(list)
 }
