669f5d23bab1681743d697c71ca8af9741256f54
Connor <cfech44@gmail.com>
2020-06-21 22:03:49 -0400

    loggin key codes

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 
 document.addEventListener("keyup", whatKey)
 
+var keyDiv = document.getElementById("keySpan")
+var keyCodeDiv = document.getElementById("keyCodeSpan")
+
 function whatKey (e){
     console.log(e.keyCode)
     console.log(e)
-console.log(e.key)
+    console.log(e.key)
+    var kc = e.keyCode
+    var k = e.key
+    keyDiv.textContent = k
+    keyCodeDiv.textContent = kc
     
 }
